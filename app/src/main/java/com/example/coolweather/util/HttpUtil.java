068b3e0f729f82fb53bfe6236e04a530029a9333
TwinkleStarrr <489385230@qq.com>
2019-12-07 14:44:55 +0800

    更新了一部分Java代码

--- app/src/main/java/com/example/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -1,12 +1,13 @@
-package com.example.coolweather.util;
+package com.example.weather.util;
 
-import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
-import android.os.Bundle;
-
-public class HttpUtil  {
-public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
-
-}
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 
 }
